472f26c06c806255cb99b3ea56c16948445521f6
brian <muriikibrian9@gmail.com>
2025-04-18 12:53:17 +0300

    Footer changes

--- frontend/src/components/layouts/AdminFooter.jsx
+++ frontend/src/components/layouts/AdminFooter.jsx
@@ -12,8 +12,18 @@ export default function AdminFooter() {
             {new Date().getFullYear()}, made with 🤎 by{" "}
             <a
               className="text-decoration-none text-dark"
+              href="https://github.com/MOGIRE11"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Thee_mmo
+            </a>{" "}
+            and{" "}
+            <a
+              className="text-decoration-none text-dark"
               href="https://github.com/Kimathi19"
-              target={"_blank"}
+              target="_blank"
+              rel="noopener noreferrer"
             >
               Kimathi
             </a>
